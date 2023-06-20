4784b1ba7a9554b74de7439cd6b839c9f7c189b0
Nader Debbabi <nader.debbabi.30@gmail.com>
2023-06-20 00:54:19 -0400

    added a simple ec2 provisioning config

---
+++ ec2/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  shared_config_files      = [ "/Users/nader/Workspace/Tools/aws-cli/.aws/config" ]
+  shared_credentials_files = [ "/Users/nader/Workspace/Tools/aws-cli/.aws/credentials" ]
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0d78d8707cd9c1be8"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
